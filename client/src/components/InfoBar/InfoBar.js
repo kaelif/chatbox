2385c20de248cd1387cbf78ea0cd67c3e9bcd73a
kaelif <flanagan.kaeli@gmail.com>
2020-06-06 20:18:43 -0700

    added info bar

---
+++ client/src/components/InfoBar/InfoBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './InfoBar.css';
+
+const InfoBar = () => {
+    <div className="indoBar">
+        <div className="leftInnerContainer">
+            <img className="onlineIcon" src={onlineIcon} />
+            <h3>roomName</h3>
+        </div>
+        <div className="rightInnerContainer">
+            <a href="/"></a>
+        </div>
+    </div>
+}
+
+export default InfoBar;
